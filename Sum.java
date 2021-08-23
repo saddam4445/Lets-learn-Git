a630d4548c8dcb6bdab8c32cfbdb97f2130537f4
Saddam Husen <saddam03121997@gmail.com>
2021-08-24 01:03:44 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args){
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println("Sum of two number"+(a+b));
     }
 }
